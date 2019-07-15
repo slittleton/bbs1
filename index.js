dd8023f462c53f8dfce6cb1f9ad6e25ff5ade148
slittleton <littletonltp@gmail.com>
2019-07-15 13:59:32 -0700

    added home route

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/users', users);
 app.use('/api/topics', topics)
 
+app.get('/', (req, res)=> {
+  res.send('HOME')
+})
+
 app.listen(PORT, console.log(`Listening on port ${PORT}`))
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 router.get('/', (req, res)=> {
-
   res.send('Users')
 })
 
